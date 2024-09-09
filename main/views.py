b57ef9b88562500f81994f0f72d331875b7429e4
dhaniatiara <dhania.t.herdiani@gmail.com>
2024-09-09 16:14:27 +0700

    new project pws

--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'name' : 'Dhania Tiaraputri Herdiani',
+        'class' : 'PBP B',
+        'NPM' : '2306165881',
         'product' : 'Peptide Lip Tint',
         'description': 'A nourishing  and tinted lip product that gives you the perfect gloss on your lips. The fragrance-free formula leaves lips feeling hydrated and visibly plump.',
         'shade' : 'Ribbon pink',
