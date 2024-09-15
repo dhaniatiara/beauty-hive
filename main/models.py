fd85f5681ba82eaa4c809ef12867a03a5e1c1e2a
dhaniatiara <dhania.t.herdiani@gmail.com>
2024-09-16 00:19:30 +0700

    created form input checklist 1

--- main/models.py
+++ main/models.py
@@ -1,12 +1,15 @@
+import uuid
 from django.db import models
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     name = models.CharField(max_length=255)
     price = models.IntegerField()
-    desciption = models.TextField()
-    shade = models.TextField()
-    size = models.TextField()
+    description = models.TextField(max_length=255)
+    shade = models.TextField(max_length=255)
+    size = models.TextField(max_length=255)
 
     @property
     def is_product_ready(self):
-        return self.mood_intensity > 5
+        return self.price > 5
--- main/views.py
+++ main/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    product_entries = Product.objects.all()
+
     context = {
-        'namamahasiswa' : 'Dhania Tiaraputri Herdiani',
-        'class' : 'PBP B',
-        'npm' : '2306165881',
         'name' : 'Peptide Lip Tint',
+        'price': '380000',
         'description': 'A nourishing  and tinted lip product that gives you the perfect gloss on your lips. The fragrance-free formula leaves lips feeling hydrated and visibly plump.',
         'shade' : 'Ribbon pink',
         'size' : '10 ml',
-        'price': '380000'
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
