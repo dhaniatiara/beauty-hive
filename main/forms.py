cb15696159e5a875b020cf1cbc92dc5bec0a88a4
dhaniatiara <dhania.t.herdiani@gmail.com>
2024-10-05 21:34:13 +0700

    tugas 5 code done

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,32 @@
 from django.forms import ModelForm
 from main.models import Product
+from django.utils.html import strip_tags
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "shade", "size"]
 
-        
+    
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        return strip_tags(name)
+
+    def clean_description(self):
+        description = self.cleaned_data["description"]
+        return strip_tags(description)
+
+   
+    def clean_price(self):
+        price = self.cleaned_data["price"]
+        return strip_tags(price)
+
+   
+    def clean_shade(self):
+        shade = self.cleaned_data["shade"]
+        return strip_tags(shade)
+
+    
+    def clean_size(self):
+        size = self.cleaned_data["size"]
+        return strip_tags(size)
--- main/views.py
+++ main/views.py
@@ -12,18 +12,20 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
+    
 
     context = {
         'name': request.user.username,
         'namamhs' : 'Dhania Tiaraputri Herdiani',
         'npm' : '2306165881',
         'class' : 'PBP B',
-        'product_entries': product_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -44,11 +46,11 @@ def create_product_entry(request):
 
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -76,11 +78,13 @@ def login_user(request):
       form = AuthenticationForm(data=request.POST)
 
       if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+      else:
+        messages.error(request, "Invalid username or password. Please try again.")
 
 
    else:
@@ -116,3 +120,25 @@ def delete_product(request, id):
     product.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = strip_tags(request.POST.get("price"))
+    description = strip_tags(request.POST.get("description"))
+    shade = strip_tags(request.POST.get("shade"))
+    size = strip_tags(request.POST.get("size"))
+    user = request.user
+
+    new_product = Product(
+        name=name,
+        price=price,
+        description=description,
+        shade=shade,
+        size=size,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
